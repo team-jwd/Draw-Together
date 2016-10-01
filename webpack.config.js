0d0ee906c3a5c43f420c5d65daf6e25f3e44a72c
medic27 <jczhang@ucla.edu>
2016-09-30 21:17:18 -0700

    added eslint with babel to build

--- webpack.config.js
+++ webpack.config.js
@@ -1,27 +1,43 @@
+const path = require('path');
+const webpack = require('webpack');
+
+// env
+const buildDirectory = './dist/';
+
 module.exports = {
   entry: [
-    './client/index.js',
+    './client/index.jsx',
   ],
-  output: {
-    path: __dirname,
-    publicPath: '/',
-    filename: 'bundle.js',
-  },
-  module: {
-    loaders: [{
-      exclude: /node_modules/,
-      loader: 'babel',
-      query: {
-        presets: ['react', 'es2015'],
-      },
-    }],
-  },
-  resolve: {
-    extensions: ['', '.js', '.jsx'],
-  },
   devServer: {
     historyApiFallback: true,
     contentBase: './',
     inline: true,
   },
+  eslint: {
+    configFile: './.eslintrc',
+  },
+  output: {
+    path: path.resolve(buildDirectory),
+    filename: 'bundle.js',
+    publicPath: 'http://localhost:8080/',
+  },
+  module: {
+    loaders: [
+      {
+        test: /\.jsx?$/,
+        exclude: /(node_modules|bower_components)/,
+        loaders: ['babel-loader', 'eslint-loader'],
+        // query: {
+        //   presets: ['react', 'es2015', 'stage-0'],
+        // },
+      },
+      {
+        test: /\.js$/,
+        loader: 'eslint-loader',
+      }
+    ],
+  },
+  resolve: {
+    extensions: ['', '.js', '.jsx'],
+  }
 };
