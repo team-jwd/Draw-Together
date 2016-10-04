dfb2a44ab6b2638e3e1db251a6210c07a32ba679
Daniel King <daniel.oliver.king@gmail.com>
2016-10-04 13:55:08 -0700

    Added actions for state transformations

--- client/actions.js
+++ client/actions.js
@@ -1,6 +1,29 @@
 export default const actions = {
+  login(username, firstName, lastName) {
+    return {type: 'LOGIN', username, firstName, lastName};
+  },
 
-  /**
-   * Put functions that return action objects here  
-   */
-}
+  logout() {
+    return {type: 'LOGOUT'}
+  },
+
+  joinRoom(roomName) {
+    return {type: 'JOIN_ROOM'}
+  },
+
+  leaveRoom() {
+    return {type: 'LEAVE_ROOM'}
+  },
+
+  addMessage(username, text) {
+    return {type: 'ADD_MESSAGE', username, text}
+  },
+
+  userJoin(username) {
+    return {type: 'USER_JOIN', username}
+  },
+
+  userLeave(username) {
+    return {type: 'USER_LEAVE', username}
+  }
+};
