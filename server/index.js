73648e0be72b0e61df62a6b404a04a399033e95d
William Barnes <barnesw91291@gmail.com>
2016-10-10 15:17:15 -0700

    switched back to http for master branch

--- server/index.js
+++ server/index.js
@@ -1,5 +1,5 @@
-const https = require('https');
-// const http = require('http');
+// const https = require('https');
+const http = require('http');
 const fs = require('fs');
 const path = require('path');
 const express = require('express');
@@ -8,7 +8,7 @@ const mongoose = require('mongoose');
 const { createSignalingChannel } = require('./signaling.js');
 
 
-const config = require('./config/config.js');
+// const config = require('./config/config.js');
 
 const PORT = process.env.PORT || 8080;
 
@@ -16,8 +16,7 @@ const userController = require('./controllers/user-controller');
 const roomController = require('./controllers/room-controller');
 
 const app = express();
-console.log("database location:", process.env.DATABASE_LOCATION);
-mongoose.connect('mongodb://localhost:80/test');
+mongoose.connect(process.env.DATABASE_LOCATION || 'mongodb://localhost:80/test');
 const db = mongoose.connection;
 
 db.on('error', console.error.bind(console, 'connection error:'));
@@ -28,14 +27,14 @@ db.once('open', () => {
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(bodyParser.json());
 
-const options = {
-  key: fs.readFileSync(path.join(`${__dirname}/config/server.key`), 'utf-8'),
-  cert: fs.readFileSync(path.join(`${__dirname}/config/server.crt`), 'utf-8'),
-  passphrase: 'boardroom',
-};
+// const options = {
+//   key: fs.readFileSync(path.join(`${__dirname}/config/server.key`), 'utf-8'),
+//   cert: fs.readFileSync(path.join(`${__dirname}/config/server.crt`), 'utf-8'),
+//   passphrase: 'boardroom',
+// };
 
-const server = https.createServer(options, app);
-// const server = http.createServer(app);
+// const server = https.createServer(options, app);
+const server = http.createServer(app);
 
 const io = require('socket.io')(server);
 
@@ -80,8 +79,6 @@ app.post('/join',
 
 
 io.on('connection', (socket) => {
-  console.log('a user connected');
-
   socket.on('save message', (msgData) => {
     roomController.saveMessage(msgData);
   });
@@ -94,7 +91,6 @@ io.on('connection', (socket) => {
   });
 
   socket.on('disconnect', () => {
-    console.log('a user disconnected');
   });
 });
 
