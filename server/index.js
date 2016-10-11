8f9f0be2b2e3b3d3ef1b8316a4f186e1a4c6e64c
Daniel King <daniel.oliver.king@gmail.com>
2016-10-11 14:35:54 -0700

    Add integration tests for server routes

--- server/index.js
+++ server/index.js
@@ -10,19 +10,38 @@ const { createSignalingChannel } = require('./signaling.js');
 
 // const config = require('./config/config.js');
 
-const PORT = process.env.PORT || 8080;
+let PORT = process.env.PORT || 8080;
+
+if (process.env.NODE_ENV === 'test') PORT = 7000;
 
 const userController = require('./controllers/user-controller');
 const roomController = require('./controllers/room-controller');
 
-const app = express();
-mongoose.connect(process.env.DATABASE_LOCATION || 'mongodb://localhost:80/test');
+let dbPath;
+if (process.env.DATABASE_LOCATION) {
+  dbPath = process.env.DATABASE_LOCATION;
+} else if (process.env.NODE_ENV === 'test') {
+  dbPath = 'mongodb://jc:zhang@ds139715.mlab.com:39715/draw-together-test';
+} else {
+  dbPath = 'mongodb://dking:helloworld@ds061325.mlab.com:61325/draw-together-development';
+}
+
+if (!mongoose.connection.host) mongoose.connect(dbPath);
+
 const db = mongoose.connection;
 
-db.on('error', console.error.bind(console, 'connection error:'));
-db.once('open', () => {
-  console.log('mongoose connected to server');
+// For testing purposes, this promise will be
+// exported so that we can avoid making requests
+// to the database before it is open.
+const dbOpen = new Promise((resolve, reject) => {
+  db.on('error', console.error.bind(console, 'connection error:'));
+  db.once('open', () => {
+    console.log('mongoose connected to server');
+    resolve();
+  });
 });
+
+const app = express();
 
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(bodyParser.json());
@@ -104,3 +123,4 @@ app.get('/', (req, res) => {
   res.sendFile(path.join(`${__dirname}/../index.html`));
 });
 
+module.exports = { app, db, dbOpen };
